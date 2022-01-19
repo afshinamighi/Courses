9ce8c394b3637a24ad750b3dacfc6b348d6321a9
afshinamighi <afshinamighi@gmail.com>
2022-01-19 10:09:40 +0100

    modified exercises for the first three weeks are uploaded

--- basecamp/week02/bc-week02-exercises/bc-w02-ex003.py
+++
@@ -1,5 +0,0 @@
-# INF-BC-Week-02
-# Exerise-001
-# Description:
-# The length of a month varies from 28 to 31 days. In this exercise you will create a program that reads the name of a month from the user as a string. Then your program should display the number of days in that month. Display “28 or 29 days”
-for February so that leap years are addressed.
